63b6d37a3688fbfecbc9ff878813a64ebc897131
Nick Jordan <35259816+NickJordan289@users.noreply.github.com>
2022-04-12 15:17:59 +0800

    Add files via upload

---
+++ .github/workflows/src/Register.js
@@ -0,0 +1,9 @@
+export default function Register() {
+  return (
+    <form>
+      <label>id:</label>
+      <input type="number" name="id" />
+
+    </form>
+  );
+}
---
+++ .github/workflows/src/data.js
@@ -0,0 +1,42 @@
+let registrations = [
+    {
+      name: "Santa Monica",
+      id: 1995,
+      amount: "$10,800",
+      due: "12/05/1995",
+    },
+    {
+      name: "Stankonia",
+      id: 2000,
+      amount: "$8,000",
+      due: "10/31/2000",
+    },
+    {
+      name: "Ocean Avenue",
+      id: 2003,
+      amount: "$9,500",
+      due: "07/22/2003",
+    },
+    {
+      name: "Tubthumper",
+      id: 1997,
+      amount: "$14,000",
+      due: "09/01/1997",
+    },
+    {
+      name: "Wide Open Spaces",
+      id: 1998,
+      amount: "$4,600",
+      due: "01/27/1998",
+    },
+  ];
+
+export function getRegistrations() {
+    return registrations;
+  }
+  
+  export function getRegistration(id) {
+    return registrations.find(
+      (registration) => registration.id === id
+    );
+  }
