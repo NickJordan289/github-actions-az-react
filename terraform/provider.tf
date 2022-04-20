5eae7278239d46362a96d795ea13ea1d4951f8b5
Nick Jordan <35259816+NickJordan289@users.noreply.github.com>
2022-04-20 15:23:28 +0800

    Update provider.tf

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -10,7 +10,6 @@ terraform {
     storage_account_name = "terraformbackendghan"
     container_name       = "terraform-state"
     key                  = "terraform.tfstate"
-    access_key           = var.BACKEND_KEY
   }
   required_providers {
     azurerm = {
