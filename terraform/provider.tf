edabe6106cdda59bdf34de033dc085cb2fd47ccd
Nick Jordan <nick.jordan@arkahna.io>
2022-04-20 14:55:27 +0800

    Added backend

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,8 +1,16 @@
 terraform {
-  backend "local" {}
+  # backend "local" {}
+  backend "azurerm" {
+    resource_group_name  = "GithubActionsNode"
+    storage_account_name = "terraformbackendghan"
+    container_name       = "terraform-state"
+    key                  = "terraform.tfstate"
+    access_key           = "h/j3hcncHPiw/vbrcrRLd3HGt0uG2jvvmp8jKIEHxtO22Kc8ywKHOyFV2YsCtkA9XJCszyOQJpdLzSGGWccxag=="
+
+  }
   required_providers {
     azurerm = {
-      source = "hashicorp/azurerm"
+      source  = "hashicorp/azurerm"
       version = "3.0.2"
     }
   }
